b5d799ea970178393923c2bbb177a26fd6531581
Pritam Barman <pritambarmanofficial1608@gmail.com>
2024-11-17 10:33:57 +0530

    just modified

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ v1 = [1,2,3]
 v2 = [1,-1,0];
 
 def dot(v1,v2):
-    v = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
-    return v
+    ve = v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+    return ve
 
 print(dot(v1,v2))
